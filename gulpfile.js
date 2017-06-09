860f3a944a0b16a44778c6c6b8633efde06be72a
zhongzhuhua <672986035@qq.com>
2017-06-09 13:40:44 +0800

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,49 @@ const autoprefixer = require('gulp-autoprefixer');
 // compressed：压缩后的css代码。
 const sass = require('gulp-sass');
 const cache = require('gulp-cache');
+let env = 'dev';
 
 gulp.task('css:pc:common', () => {
   gulp.src('www/assets/css/common/**/*.pc.scss')
-    .pipe(autoprefixer({
-      browsers: ['ie >= 8', 'Firefox > 10', 'chrome>1.0'],
-      cascade: false
-    }))
-    .pipe(sass({
-      outputStyle: 'compact'
-    }))
+    .pipe(cache(autoprefixer(configs.cssPC)))
+    .pipe(sass(configs.cssOutput))
     .pipe(gulp.dest('www/static/common/css'))
 });
 
 gulp.task('css:mm:common', () => {
   gulp.src('www/assets/css/common/**/*.mm.scss')
-    .pipe(autoprefixer({
-      browsers: ['Android >= 2.1', 'IOS >= 5.0'],
-      cascade: false
-    }))
-    .pipe(sass({
-      outputStyle: 'compact'
-    }))
+    .pipe(cache(autoprefixer(configs.cssMM)))
+    .pipe(sass(configs.cssOutput))
     .pipe(gulp.dest('www/static/common/css'))
 });
 
-gulp.task('default', ['css:pc:common', 'css:mm:common']);
+// 任务
+let configs = {
+  // 打包公用样式
+  commonCss: ['css:pc:common', 'css:mm:common'],
+  // pc autoprefixer 兼容
+  cssPC: {
+    browsers: ['ie >= 8', 'Firefox > 10', 'chrome>1.0'],
+    cascade: false
+  },
+  // mobile autoprefixer 兼容
+  cssMM: {
+    browsers: ['Android >= 2.1', 'IOS >= 5.0'],
+    cascade: false
+  },
+  // 样式压缩格式
+  cssOutput: {
+    outputStyle: env == 'dev' ? 'expanded' : 'compressed'
+  }
+};
+
+// 开发
+gulp.task('default', configs.commonCss);
+gulp.task('watch', () => {
+  gulp.watch('./www/assets/css/**/*.scss', configs.commonCss);
+});
+
+gulp.task('build', () => {
+  env = 'prd';
+  gulp.run(configs.commonCss);
+});
--- src/front/controller/base.js
+++ src/front/controller/base.js
@@ -4,13 +4,14 @@ export default class extends think.controller.base {
   init(http) {
     super.init(http);
     this.env = this.config('env');
+    this.DeviceWidth = 720;
+    this.meta_title = '首页';
   };
 
   /** 
    */
   goto(type) {
     let myroute = type == null ? (!checkMobile(this.userAgent()) ? '' : '.mm.html') : type == 'mobile' ? '.mm.html' : '';
-    console.log(`${this.http.controller}/${this.http.action}${myroute}`)
     return `${this.http.controller}/${this.http.action}${myroute}`;
   };
 };
